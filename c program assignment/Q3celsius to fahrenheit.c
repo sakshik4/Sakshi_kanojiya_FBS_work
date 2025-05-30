23817dedc3da67d9985926c07dc79bcf17832050
Sakshi Kanojiya <153350046+sakshik4@users.noreply.github.com>
2025-05-30 23:15:20 +0530

    Add files via upload

---
+++ c program assignment/Q3celsius to fahrenheit.c
@@ -0,0 +1,7 @@
+void main()
+{
+	float celsius,fahrenheit;
+	celsius=21.5;
+	fahrenheit=celsius*1.8+32;
+	printf("Convert celsius to fahrenheit %f",fahrenheit);
+	}
---
+++ c program assignment/Q4 swapping.c
@@ -0,0 +1,10 @@
+void main(){
+	int a,b,temp;
+	a=10;
+	b=20;
+	temp;
+	temp=a;
+	a=b;
+	b=temp;
+	printf("After swpping of a and b values are %d,%d",a,b);
+}
